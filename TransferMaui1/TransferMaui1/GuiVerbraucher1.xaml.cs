9c16b72f76434410f8f22e7bf43b140520de80f1
Michael Thomas <129625504+Mitho33@users.noreply.github.com>
2023-11-30 10:24:17 +0100

    TransferMaui1
    TransferMaui1

---
+++ TransferMaui1/TransferMaui1/GuiVerbraucher1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TransferMaui1
+{
+    /// <summary>
+    /// Interaktionslogik für GuiVerbraucher1.xaml
+    /// </summary>
+    public partial class GuiVerbraucher1 : Window
+    {
+        public GuiVerbraucher1()
+	{
+		InitializeComponent();
+        LV1.ItemsSource = gl.Preisliste;
+    }
+
+    ViewModel1 gl = new ViewModel1();
+
+
+        private void BtnClose(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
